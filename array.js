6bc20fbcff54486ad6889aa6f952d02d6f904231
anikmondol <anikmondol558363@gmil.com>
2024-03-30 15:37:47 +0600

    this video about more js conspts

---
+++ array.js
@@ -0,0 +1,14 @@
+// const products = [
+//     {name: 'phone', price: 12000},
+//     {name: 'laptop', price: 12000},
+// ]
+
+
+// products[0]
+// products[1]
+
+// const products = [15, 56];
+const products = {
+    '0': 15,
+    '1': 56
+}
